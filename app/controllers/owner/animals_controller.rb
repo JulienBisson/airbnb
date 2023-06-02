8c5c6bed9a92b8013bb4214b09f53838f2e92f1d
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-06-02 14:54:42 +0200

    create owner:animal#show

--- app/controllers/owner/animals_controller.rb
+++ app/controllers/owner/animals_controller.rb
@@ -9,5 +9,26 @@ module Owner
     def new
       @animal = Animal.new
     end
+
+    def show
+      @animal = Animal.find(params[:id])
+    end
+
+    def create
+      @animal = Animal.new(owner_params)
+      @animal.owner = current_user
+      # @booking.total_price = (@booking.end_date - @booking.start_date) * @animal.price_per_day
+      if @animal.save
+        redirect_to animal_path(@animal), notice: "Beast adding ✔️"
+      else
+        render 'animals/index', status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def owner_params
+      params.require(:animal).permit(:name, :price_per_day, :localisation, :can_swim, :can_walk, :can_fly, :summary, :photo)
+    end
   end
 end
